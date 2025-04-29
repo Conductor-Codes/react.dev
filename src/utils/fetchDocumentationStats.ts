9a96ec2df2b13946934acf76bc1b815d8b7ca5f7
RobOBrien <rdo50181@gmail.com>
2025-04-29 12:25:11 -0400

    remove comments

--- src/utils/fetchDocumentationStats.ts
+++ src/utils/fetchDocumentationStats.ts
@@ -1,17 +1,4 @@
 import documentationStats from '../data/documentationStats.json';
-
-/**
- * Synchronously loads documentation analytics data
- * This function is called at import/module load time
- * to preload the data before rendering
- */
 export function fetchDocumentationStats() {
-  // In a real application, this might include more complex logic:
-  // - Transforming data
-  // - Merging from multiple sources
-  // - Calculating derived values
-  // - Filtering out irrelevant entries
-
-  // For now, we simply return the data
   return documentationStats;
 }
